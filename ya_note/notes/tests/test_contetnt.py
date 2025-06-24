93ddc8263da2685b47287756849deba1ba3a5622
Илья Вилков <enot19vilkov@yandex.ru>
2025-06-24 09:57:57 +0300

    correct content Unitest

--- ya_note/notes/tests/test_contetnt.py
+++ ya_note/notes/tests/test_contetnt.py
@@ -1,46 +1,26 @@
-from django.test import TestCase, Client
-from django.contrib.auth import get_user_model
-from django.urls import reverse
-
 from notes.forms import NoteForm
-from notes.models import Note
+from notes.tests.base_test_case import (BaseTest,
+                                        LIST_URL,
+                                        ADD_URL,
+                                        EDIT_URL)
 
-User = get_user_model()
 
-
-class TestContent(TestCase):
-
-    @classmethod
-    def setUpTestData(cls):
-        cls.author = User.objects.create(
-            username='Илья Вилков'
-        )
-        cls.note = Note.objects.create(title='Заголовок',
-                                       text='Текст', author=cls.author)
-        cls.reader = User.objects.create(username='Илья')
-        cls.author_client = Client()
-        cls.author_client.force_login(cls.author)
-        cls.reader_client = Client()
-        cls.reader_client.force_login(cls.reader)
-        cls.add_url = reverse('notes:add')
-        cls.edit_url = reverse('notes:edit', args=(cls.note.slug,))
+class TestContent(BaseTest):
 
     def test_notes_list_for_auth_user(self):
-        url = reverse('notes:list')
-        response = self.author_client.get(url)
+        response = self.author_client.get(LIST_URL)
         notes = response.context['object_list']
         self.assertIn(self.note, notes)
 
     def test_notes_list_for_anonymous_user(self):
-        url = reverse('notes:list')
-        response = self.reader_client.get(url)
+        response = self.reader_client.get(LIST_URL)
         notes = response.context['object_list']
         self.assertNotIn(self.note, notes)
 
     def test_create_and_add_note_pages_contains_form(self):
         urls = (
-            (self.add_url),
-            (self.edit_url)
+            (ADD_URL),
+            (EDIT_URL)
         )
         for url in urls:
             response = self.author_client.get(url)
